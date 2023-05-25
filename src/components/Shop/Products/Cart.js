887fc4a77594a59faaf2a03137b2223eb200c039
natalia646 <nataxash46@gmail.com>
2023-05-25 13:24:33 +0300

    first page

--- src/components/Shop/Products/Cart.js
+++ src/components/Shop/Products/Cart.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function Cart() {
+    
   return (
-    <div>
-      <img></img>
-      <p></p>
-      <button></button>
+    <div className='productCart'>
+      <img src='https://img.freepik.com/free-photo/tasty-burger-isolated-on-white-background-fresh-hamburger-fastfood-with-beef-and-cheese_90220-1063.jpg?size=626&ext=jpg'></img>
+      <p>Burger</p>
+      <button>Add to cart</button>
     </div>
   )
 }
---
+++ src/components/Shop/Products/Carts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cart from './Cart'
+
+export default function Carts() {
+  return (
+    <div className='carts'>
+      <Cart/>
+    </div>
+  )
+}
